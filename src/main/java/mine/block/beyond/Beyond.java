e309c303c1eca36876c5e65c64470bb367bb4eb7
Calum <93472213+cal117@users.noreply.github.com>
2022-07-15 23:27:22 +0100

    The Start

---
+++ src/main/java/mine/block/beyond/Beyond.java
@@ -0,0 +1,19 @@
+package mine.block.beyond;
+
+import net.fabricmc.api.ModInitializer;
+import net.kyrptonaught.customportalapi.api.CustomPortalBuilder;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.Items;
+import net.minecraft.util.Identifier;
+
+public class Beyond implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        CustomPortalBuilder.beginPortal()
+                .frameBlock(Blocks.REINFORCED_DEEPSLATE)
+                .destDimID(new Identifier("the_end"))
+                .tintColor(5, 98, 93)
+                .forcedSize(20, 6)
+                .registerPortal();
+    }
+}
