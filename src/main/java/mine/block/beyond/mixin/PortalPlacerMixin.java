57a7a6541b939677d2db2593418adde9426646d9
Calum <93472213+cal117@users.noreply.github.com>
2022-07-16 22:41:06 +0100

    fix spawn events

--- src/main/java/mine/block/beyond/mixin/PortalPlacerMixin.java
+++ src/main/java/mine/block/beyond/mixin/PortalPlacerMixin.java
@@ -1,22 +1,19 @@
 package mine.block.beyond.mixin;
 
 import mine.block.beyond.data.BeyondAdvancements;
-import mine.block.beyond.data.critereon.SpawnedPortalCritereon;
 import net.kyrptonaught.customportalapi.portal.PortalIgnitionSource;
 import net.kyrptonaught.customportalapi.portal.PortalPlacer;
-import net.minecraft.block.BeehiveBlock;
 import net.minecraft.block.Blocks;
-import net.minecraft.block.SculkShriekerBlock;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LargeEntitySpawnHelper;
 import net.minecraft.entity.SpawnReason;
-import net.minecraft.entity.ai.TargetPredicate;
 import net.minecraft.entity.mob.WardenEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.ServerWorldAccess;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.math.Vec3i;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.BiomeKeys;
 import org.spongepowered.asm.mixin.Mixin;
@@ -27,6 +24,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(PortalPlacer.class)
 public class PortalPlacerMixin {
 
+
+
+    private static void trySpawnWarden(ServerWorld world, BlockPos pos) {
+        WardenEntity entity = LargeEntitySpawnHelper.trySpawnAt(EntityType.WARDEN, SpawnReason.TRIGGERED, world, pos, 20, 15, 10, LargeEntitySpawnHelper.Requirements.WARDEN).get();
+    }
+
     @Inject(method = "attemptPortalLight(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/math/BlockPos;Lnet/kyrptonaught/customportalapi/portal/PortalIgnitionSource;)Z", at = @At("RETURN"), cancellable = false)
     private static void onPortalLight(World world, BlockPos portalPos, BlockPos framePos, PortalIgnitionSource ignitionSource, CallbackInfoReturnable<Boolean> cir) {
         if(!cir.getReturnValue() || !(world.getBlockState(framePos) == Blocks.REINFORCED_DEEPSLATE.getDefaultState()) || world.getBiome(framePos).getKey().get() != BiomeKeys.DEEP_DARK) {
@@ -34,8 +37,10 @@ public class PortalPlacerMixin {
         }
 
         for (int i = 0; i < 9; i++) {
-            LargeEntitySpawnHelper.trySpawnAt(EntityType.WARDEN, SpawnReason.TRIGGERED, (ServerWorld) world, portalPos, 20, 15, 10, LargeEntitySpawnHelper.Requirements.WARDEN);
+            trySpawnWarden((ServerWorld) world, framePos);
         }
+
+        WardenEntity.addDarknessToClosePlayers((ServerWorld) world, Vec3d.ofCenter(portalPos.add(new Vec3i(5, -3, 5))), null, 40);
 
 
         var entitys = world.getPlayers();
